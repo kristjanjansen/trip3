5575e65b1262c152923b378dfc3e32c5ef4949ba
kristjanjansen <kristjan.jansen@gmail.com>
2021-04-09 16:45:26 +0300

    feat: adding grid

--- resources/js/layouts/Main.vue
+++ resources/js/layouts/Main.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { useMagicKeys, useStorage, whenever } from "@vueuse/core";
+
+const keys = useMagicKeys();
+
+const showGrid = useStorage("trip_grid", false);
+
+whenever(keys.shift_g, () => {
+    showGrid.value = !showGrid.value;
+});
+</script>
+
 <template>
     <slot />
+    <basegrid v-if="showGrid" />
 </template>
