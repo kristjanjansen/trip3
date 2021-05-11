20c105f9244d08533e05cf2d4c1ad175ba081a50
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-11 11:34:30 +0300

    models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Content;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //$users = User::factory(2)->create();
+        Content::factory(10)->create();
         Content::factory(10)
-            //->for($users->random())
+            ->hasComments(3)
             ->create();
     }
 }
