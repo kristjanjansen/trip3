0245b31cdf37c97f3810431a068a630c21a5c570
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-12 23:22:54 +0300

    cleanup

--- resources/js/models.ts
+++ resources/js/models.ts
@@ -1,3 +1,50 @@
+import router from "ziggy-js";
+import { trans, __ } from "./utils";
+import { InertiaLink } from "@inertiajs/inertia-vue3";
+
+declare module "vue" {
+    export interface ComponentCustomProperties {
+        route: typeof router;
+        trans: typeof trans;
+        __: typeof __;
+    }
+    export interface GlobalComponents {
+        InertiaLink: InertiaLink;
+        BackgroundGrid: typeof import("./components/BackgroundGrid.vue").default;
+        BlockTitle: typeof import("./components/BlockTitle.vue").default;
+        Button: typeof import("./components/Button.vue").default;
+        ButtonLink: typeof import("./components/ButtonLink.vue").default;
+        Comment: typeof import("./components/Comment.vue").default;
+        CommentCreateForm: typeof import("./components/CommentCreateForm.vue").default;
+        CommentEditForm: typeof import("./components/CommentEditForm.vue").default;
+        Comments: typeof import("./components/Comments.vue").default;
+        FieldLayout: typeof import("./components/FieldLayout.vue").default;
+        Footer: typeof import("./components/Footer.vue").default;
+        FormTextarea: typeof import("./components/FormTextarea.vue").default;
+        FormTextfield: typeof import("./components/FormTextfield.vue").default;
+        ForumHeader: typeof import("./components/ForumHeader.vue").default;
+        ForumLayout: typeof import("./components/ForumLayout.vue").default;
+        ForumPost: typeof import("./components/ForumPost.vue").default;
+        ForumRow: typeof import("./components/ForumRow.vue").default;
+        IconClose: typeof import("./components/IconClose.vue").default;
+        IconMenu: typeof import("./components/IconMenu.vue").default;
+        IconPin: typeof import("./components/IconPin.vue").default;
+        IconRightarrow: typeof import("./components/IconRightarrow.vue").default;
+        IconRss: typeof import("./components/IconRss.vue").default;
+        IconSearch: typeof import("./components/IconSearch.vue").default;
+        IconThumbsdown: typeof import("./components/IconThumbsdown.vue").default;
+        IconThumbsup: typeof import("./components/IconThumbsup.vue").default;
+        IconTwitter: typeof import("./components/IconTwitter.vue").default;
+        IconUser: typeof import("./components/IconUser.vue").default;
+        Logo: typeof import("./components/Logo.vue").default;
+        LogoPlain: typeof import("./components/LogoPlain.vue").default;
+        Nav: typeof import("./components/Nav.vue").default;
+        Tag: typeof import("./components/Tag.vue").default;
+        TransitionFade: typeof import("./components/TransitionFade.vue").default;
+        TransitionGroupFade: typeof import("./components/TransitionGroupFade.vue").default;
+    }
+}
+
 export type Comment = {
     id: number;
     user_id: number;
--- routes/console.php
+++ routes/console.php
@@ -18,10 +18,48 @@ use Illuminate\Support\Facades\Storage;
 
 Artisan::command("generate:types", function () {
     $this->call("types:generate");
-    $models = collect(Storage::allFiles("resources/js/models"));
-    $f = $models
+
+    $models = collect(Storage::files("resources/js/models"));
+    $components = collect(Storage::files("resources/js/components"));
+    $components_async = collect(
+        Storage::files("resources/js/components_async")
+    );
+
+    $c = $components
+        ->map(fn($path) => basename($path, ".vue"))
+        ->map(
+            fn($name) => "        " .
+                $name .
+                ': typeof import("./components/' .
+                $name .
+                '.vue").default;'
+        )
+        ->join("\n");
+
+    $m = $models
         ->map(fn($path) => Storage::get($path))
         ->map(fn($model) => "export " . $model)
         ->join("\n");
-    Storage::put("/resources/js/models.ts", $f);
+
+    $template = <<<END
+import router from "ziggy-js";
+import { trans, __ } from "./utils";
+import { InertiaLink } from "@inertiajs/inertia-vue3";
+
+declare module "vue" {
+    export interface ComponentCustomProperties {
+        route: typeof router;
+        trans: typeof trans;
+        __: typeof __;
+    }
+    export interface GlobalComponents {
+        InertiaLink: InertiaLink;
+$c
+    }
+}
+
+$m;
+END;
+
+    Storage::put("/resources/js/models.ts", $template);
 });
