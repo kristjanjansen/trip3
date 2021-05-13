20474f1167cb4f37c9dcf210862c6987a84be1b8
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-13 22:20:21 +0300

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @production
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    @endproduction
     <link rel="preload" href="/fonts/Regular.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="/fonts/RegularItalic.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="/fonts/Medium.woff2" as="font" type="font/woff2" crossorigin>
