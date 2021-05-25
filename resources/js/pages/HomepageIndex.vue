e22b5d84e3e2e1889d69ef97b18ad6796eff21e7
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-25 17:00:33 +0300

    cleanup

--- resources/js/pages/HomepageIndex.vue
+++ resources/js/pages/HomepageIndex.vue
@@ -3,7 +3,7 @@
         <div class="max-w-screen-xl mx-auto">
             <Nav />
         </div>
-        <Image filename="header.jpg" class="object-cover h-64 w-full" />
+        <Image filename="header.jpg" class="object-cover h-96 w-full" />
         <Footer />
     </div>
 </template>
